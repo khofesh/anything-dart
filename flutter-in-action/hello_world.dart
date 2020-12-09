9b5fca944cb91381e41342d15dd00ee2b033d83e
Fahmi Ahmad <fahmiahmad@tutanota.com>
2020-12-09 18:40:47 +0700

    hello

---
+++ flutter-in-action/hello_world.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void helloDart(String name) {
+  print('Hello, $name!');
+}
+
+void main() {
+  List<String> names = [
+    'World',
+    'Mars',
+    'Oregon',
+    'Barry',
+    'David Bowie',
+  ];
+
+  for (var name in names) {
+    helloDart(name);
+  }
+
+  stdout.writeln('Greet somebody');
+  String input = stdin.readLineSync();
+  return helloDart(input);
+}
